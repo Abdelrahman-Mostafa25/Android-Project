7879d898b13cbb0555247d0444db99398fcd0195
Abdulrahman-Awad21 <abdulrahman.awad21@gmail.com>
2022-04-14 03:50:53 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/project_sci/MainActivity.java
+++ app/src/main/java/com/example/project_sci/MainActivity.java
@@ -68,6 +68,7 @@ public class    MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     //Notification Methods +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
